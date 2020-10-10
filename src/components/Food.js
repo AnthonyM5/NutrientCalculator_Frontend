f45192f42fd0a59a8f88c75cd2618dcc0874c887
antimonysayz <antimonysayz@gmail.com>
2020-10-09 23:46:03 -0400

    nutrient_hash saves correctly

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,12 +1,12 @@
 class Food {
     constructor(food){
         this.name = food.name
-        this.nutrient_hash = food.nutrient_hash
+        this.nutrient_hash = JSON.stringify(food.nutrient_hash)
         this.render()
     }
 
     render(){
-    console.log(`${this.name}`)
+    console.log(`${this.nutrient_hash}`)
     const li = document.createElement("li")
     li.innerText = this.name
     const body = document.querySelector('body')
