f7bc2b4eb4eb4d35b7d644236bddcd674537e81b
antimonysayz <antimonysayz@gmail.com>
2020-10-19 23:59:22 -0400

    adding ingredient rerenders the same meal

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -3,13 +3,15 @@ class Ingredient {
         this.food_id = food
         this.name = food.name
         this.science_name = food.science
-        this.addToMeal()
         this.meal_id = state.meal
+        this.addToMeal()
+        
     }
 
     addToMeal(){
-        console.log(this)
+        console.log(this.meal_id)
         // api.addToMeal(1, this.food_id)
+        new MealPages(this.meal_id)
     }
 
 
